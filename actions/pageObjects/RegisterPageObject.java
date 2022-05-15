062d6cee7e9d0c3d823b7ffd283dc76434bb277d
PhanThanhTruc <phanthithanhtruc98@gmail.com>
2022-05-15 12:28:39 +0700

    Commit Test Case Page Object Pattern - Part I

--- actions/pageObjects/RegisterPageObject.java
+++ actions/pageObjects/RegisterPageObject.java
@@ -1,5 +1,92 @@
 package pageObjects;
 
-public class RegisterPageObject {
+import org.openqa.selenium.WebDriver;
+
+import commons.BasePage;
+import pageUIs.RegisterPageUI;
+
+public class RegisterPageObject extends BasePage {
+	private WebDriver driver;
+
+	public RegisterPageObject(WebDriver driver) {
+		super();
+		this.driver = driver;
+	}
+
+	public void clickToRegisterButton() {
+		waitForElementClickAble(driver, RegisterPageUI.REGISTER_BUTTON);
+		clickToElement(driver, RegisterPageUI.REGISTER_BUTTON);
+	}
+
+	public String getErrorMessageAtFirstNameTextbox() {
+		waitForElementVisible(driver, RegisterPageUI.FIRST_NAME_ERROR_MSG);
+		return getElementText(driver, RegisterPageUI.FIRST_NAME_ERROR_MSG);
+	}
+
+	public String getErrorMessageAtLastNameTextbox() {
+		waitForElementVisible(driver, RegisterPageUI.LAST_NAME_ERROR_MSG);
+		return getElementText(driver, RegisterPageUI.LAST_NAME_ERROR_MSG);
+	}
+
+	public String getErrorMessageAtEmailTextbox() {
+		waitForElementVisible(driver, RegisterPageUI.EMAIL_ERROR_MSG);
+		return getElementText(driver, RegisterPageUI.EMAIL_ERROR_MSG);
+	}
+
+	public String getErrorMessageAtPasswordTextbox() {
+		waitForElementVisible(driver, RegisterPageUI.PASSWORD_ERROR_MSG);
+		return getElementText(driver, RegisterPageUI.PASSWORD_ERROR_MSG);
+	}
+
+	public String getErrorMessageAtConfirmPasswordTextbox() {
+		waitForElementVisible(driver, RegisterPageUI.CONFIRM_PASSWORD_ERROR_MSG);
+		return getElementText(driver, RegisterPageUI.CONFIRM_PASSWORD_ERROR_MSG);
+	}
+
+	public String getErrorMessageExistEmail() {
+		waitForElementVisible(driver, RegisterPageUI.EMAIL_EXIST_ERROR_MSG);
+		return getElementText(driver, RegisterPageUI.EMAIL_EXIST_ERROR_MSG);
+	}
+
+	public String getRegisterSuccessMessage() {
+		waitForElementVisible(driver, RegisterPageUI.REGISTER_SUCCESS);
+		return getElementText(driver, RegisterPageUI.REGISTER_SUCCESS);
+	}
+
+	public void inputToFirstNameTextbox(String firstName) {
+		waitForElementVisible(driver, RegisterPageUI.FIRST_NAME_TEXTBOX);
+		sendKeyToElement(driver, RegisterPageUI.FIRST_NAME_TEXTBOX, firstName);
+
+	}
+
+	public void inputToLastNameTextbox(String lastName) {
+		waitForElementVisible(driver, RegisterPageUI.LAST_NAME_TEXTBOX);
+		sendKeyToElement(driver, RegisterPageUI.LAST_NAME_TEXTBOX, lastName);
+
+	}
+
+	public void inputToEmailTextbox(String email) {
+		waitForElementVisible(driver, RegisterPageUI.EMAIL_TEXTBOX);
+		sendKeyToElement(driver, RegisterPageUI.EMAIL_TEXTBOX, email);
+
+	}
+
+	public void inputToConfirmPasswordTextbox(String confirmPassword) {
+		waitForElementVisible(driver, RegisterPageUI.CONFIRM_PASSWORD_TEXTBOX);
+		sendKeyToElement(driver, RegisterPageUI.CONFIRM_PASSWORD_TEXTBOX, confirmPassword);
+
+	}
+
+	public void inputToPasswordTextbox(String password) {
+		waitForElementVisible(driver, RegisterPageUI.PASSWORD_TEXTBOX);
+		sendKeyToElement(driver, RegisterPageUI.PASSWORD_TEXTBOX, password);
+
+	}
+
+	public void clickToLogoutLink() {
+		waitForElementClickAble(driver, RegisterPageUI.LOGOUT_LINK);
+		clickToElement(driver, RegisterPageUI.LOGOUT_LINK);
+
+	}
 
 }
---
+++ testcases/com/nopcommerce/user/Level_03_Page_Object.java
@@ -0,0 +1,152 @@
+package com.nopcommerce.user;
+
+import org.testng.annotations.Test;
+
+import commons.BasePage;
+import pageObjects.HomePageObject;
+import pageObjects.RegisterPageObject;
+
+import org.testng.annotations.BeforeClass;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+
+public class Level_03_Page_Object extends BasePage {
+	private WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+	private String firstName, lastName, email, pass;
+	HomePageObject homePage;
+	RegisterPageObject registerPage;
+
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get("https://demo.nopcommerce.com/");
+		
+		firstName="test";
+		lastName="tester";
+		email = "Abc" + ranDom() + "@gmail.com";
+		pass="Abc@12345";
+		
+		homePage = new HomePageObject(driver);
+		registerPage = new RegisterPageObject(driver);
+
+	}
+
+	@Test
+	public void TC_01_Register_Emplty() {
+
+		homePage.clickToRegisterLink();
+
+		registerPage.clickToRegisterButton();
+
+		Assert.assertEquals(registerPage.getErrorMessageAtFirstNameTextbox(), "First name is required.");
+		Assert.assertEquals(registerPage.getErrorMessageAtLastNameTextbox(), "Last name is required.");
+		Assert.assertEquals(registerPage.getErrorMessageAtEmailTextbox(), "Email is required.");
+		Assert.assertEquals(registerPage.getErrorMessageAtPasswordTextbox(), "Password is required.");
+		Assert.assertEquals(registerPage.getErrorMessageAtConfirmPasswordTextbox(), "Password is required.");
+
+	}
+
+	@Test
+	public void TC_02_Invalid_Email() {
+		homePage.clickToRegisterLink();
+
+		registerPage.inputToFirstNameTextbox(firstName);
+		registerPage.inputToLastNameTextbox(lastName);
+		registerPage.inputToEmailTextbox("Abc123");
+		registerPage.inputToPasswordTextbox(pass);
+		registerPage.inputToConfirmPasswordTextbox(pass);
+
+		registerPage.clickToRegisterButton();
+
+		Assert.assertEquals(registerPage.getErrorMessageAtEmailTextbox(), "Wrong email");
+	}
+
+	@Test
+	public void TC_03_Success() {
+		homePage.clickToRegisterLink();
+
+		registerPage.inputToFirstNameTextbox(firstName);
+		registerPage.inputToLastNameTextbox(lastName);
+		registerPage.inputToEmailTextbox(email);
+		registerPage.inputToPasswordTextbox(pass);
+		registerPage.inputToConfirmPasswordTextbox(pass);
+
+		registerPage.clickToRegisterButton();
+
+		Assert.assertEquals(registerPage.getRegisterSuccessMessage(), "Your registration completed");
+
+		registerPage.clickToLogoutLink();
+
+	}
+
+	@Test
+	public void TC_04_Register_Existing_Email() {
+		homePage.clickToRegisterLink();
+
+		registerPage.inputToFirstNameTextbox(firstName);
+		registerPage.inputToLastNameTextbox(lastName);
+		registerPage.inputToEmailTextbox(email);
+		registerPage.inputToPasswordTextbox(pass);
+		registerPage.inputToConfirmPasswordTextbox(pass);
+
+		registerPage.clickToRegisterButton();
+
+		Assert.assertEquals(registerPage.getErrorMessageExistEmail(), "The specified email already exists");
+
+	}
+
+	@Test
+	public void TC_05_Register_Pass_Less_Than_6_Chars() {
+		homePage.clickToRegisterLink();
+
+		registerPage.inputToFirstNameTextbox(firstName);
+		registerPage.inputToLastNameTextbox(lastName);
+		registerPage.inputToEmailTextbox(email);
+		registerPage.inputToPasswordTextbox("Abc12");
+		registerPage.inputToConfirmPasswordTextbox(pass);
+
+		registerPage.clickToRegisterButton();
+
+		Assert.assertEquals(registerPage.getErrorMessageAtPasswordTextbox(),
+				"Password must meet the following rules:\nmust have at least 6 characters");
+
+	}
+
+	@Test
+	public void TC_06_Register_Invalid_COnfirm_Pass() {
+		homePage.clickToRegisterLink();
+
+		registerPage.inputToFirstNameTextbox(firstName);
+		registerPage.inputToLastNameTextbox(lastName);
+		registerPage.inputToEmailTextbox(email);
+		registerPage.inputToPasswordTextbox(pass);
+		registerPage.inputToConfirmPasswordTextbox("Abc@123");
+
+		registerPage.clickToRegisterButton();
+
+		Assert.assertEquals(registerPage.getErrorMessageAtConfirmPasswordTextbox(),
+				"The password and confirmation password do not match.");
+
+	}
+
+	public int ranDom() {
+		Random rand = new Random();
+		return rand.nextInt(9999);
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+}
